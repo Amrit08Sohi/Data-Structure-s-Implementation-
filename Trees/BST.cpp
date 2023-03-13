ab853efba8e45df76b3cb6f64c713a6fd6d229e0
Amrit-Sohi <sohisaab1623@gmail.com>
2023-03-13 22:16:06 +0530

    Added BST code

---
+++ Trees/BST.cpp
@@ -0,0 +1,128 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) {
+            this->data = data;
+            this->left = this->right = NULL;
+        }
+};
+
+class BST {
+    public:
+        Node* root;
+
+        BST() {
+            root = NULL;
+        }
+
+        Node* insert(Node* root,int value) {
+            if(root == NULL) {
+                root = new Node(value);
+                return root;
+            }
+
+            if(root->data > value) {
+                root->left = insert(root->left,value);
+            }else {
+                root->right = insert(root->right,value);
+            }
+            return root;
+        }
+
+        void inorder(Node* root) {
+            if(root == NULL) {
+                return;
+            }
+            inorder(root->left);
+            cout << root->data << " ";
+            inorder(root->right);
+        }
+
+        Node* deleteNode(Node* root,int val) {
+            if(root == NULL) {
+                return root;
+            }
+
+            if(root->data > val) {
+                root->left = deleteNode(root->left,val);
+            } else if(root->data < val) {
+                root->right = deleteNode(root->right,val);
+            } else { // if(root.data == val )
+                // Case 1 : For leaf node
+                if(root->left == NULL && root->right == NULL) {
+                    return NULL;
+                }
+
+                // Case 2: For one child
+                if(root->left == NULL) {
+                    return root->right;
+                } else if (root->right == NULL) {
+                    return root->left;
+                }
+
+                // Case 3 : For Two child
+                // 1. Find Inorder Successor
+                Node* IS = findIS(root->right);
+                cout << "Find IS" << endl;
+                // 2. Replace value with inorder successor
+                root->data = IS->data;
+                // 3. Delete the Node of Onorder Successor
+                root->right = deleteNode(root->right,IS->data);
+            }
+            return root;
+        }
+
+        Node* findIS(Node* root) {
+            while(root->left != NULL) {
+                root = root->left;
+            }
+            return root;
+        }
+
+        bool searchKey(Node* root,int key) {
+            if(root == NULL) {
+                return false;
+            }
+
+            if(root->data == key) {
+                return true;
+            }
+            if(root->data > key) {
+                return searchKey(root->left,key);
+            } else  {
+                return searchKey(root->right,key);
+            }
+            
+        }
+
+
+};
+int main()
+{
+    int values[] = {5,1,3,4,2,7};
+    /*
+        BST :
+                    5
+                  /   \
+                 1     7   
+                  \  
+                   3
+                 /  \  
+                2    4
+    */
+
+    BST* tree = new BST();
+    for(int ele: values) {
+        tree->root = tree->insert(tree->root,ele);
+    }
+    tree->root = tree->deleteNode(tree->root,1);
+    tree->inorder(tree->root);
+    cout << endl;
+    cout << tree->searchKey(tree->root,4);
+    return 0;
+}
